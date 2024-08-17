8f9b234080c1c2d4deeba01348b1074920808db4
bobby.liu <fellowe5@hotmail.com>
2024-08-17 13:59:43 +0800

    Added effect to <Button> in AddAvailabilityButton component

--- src/components/AddAvailabilityButton.tsx
+++ src/components/AddAvailabilityButton.tsx
@@ -3,11 +3,12 @@ import { useContext } from "react";
 import { ModeContext } from "@/app/theme-provider";
 
 const AddAvailabilityButton = () => {
-  const { mode, setMode } = useContext(ModeContext);
+  const { mode, setMode, effect } = useContext(ModeContext);
   return (
     <>
       {mode === "read" && (
         <Button
+          key={effect}
           className="lg:mt-0 lg:animate-bounce-awhile w-full h-12 lg:h-10"
           onClick={() => {
             console.log("Clicked add availability");
